d40bb2cd48c27c03759c2d264db25c0a53c5195f
Jordu fi <jordufi@gmail.com>
2023-09-12 18:23:45 +0200

    ahora funciona con teclas

--- FlashcardsPrueba.py
+++ FlashcardsPrueba.py
@@ -16,7 +16,6 @@ flashcards = [
 ]
 import tkinter as tk
 from PIL import Image, ImageTk
-
 class FlashcardApp:
     def __init__(self, root):
         self.root = root
@@ -32,53 +31,36 @@ class FlashcardApp:
         self.flashcard_image2 = self.load_and_resize_image("F:/programacion/flashcards/images/NotasPentagrama/Cuadrado.png")
         self.flashcard_label2 = tk.Label(root, image=self.flashcard_image2)
         self.flashcard_label2.pack()
-        
-        self.root.bind("<Right>", self.next_question)
-        self.root.bind("<space>", self.show_answer)
-        
 
         self.show_answer_button = tk.Button(root, text="Mostrar Respuesta", command=self.show_answer)
-        #self.show_answer_button.place(x=10, y=810)  # Posición fija para el botón
         self.show_answer_button.pack()
 
         self.next_question_button = tk.Button(root, text="Siguiente Pregunta", command=self.next_question)
-        #self.next_question_button.place(x=150, y=810)  # Posición fija para el botón
         self.next_question_button.pack()
+
+        # Configura la vinculación de eventos de teclado
+        self.root.bind("<Right>", self.next_question)
+        self.root.bind("<space>", self.show_answer)
 
     def load_and_resize_image(self, image_path):
         original_image = Image.open(image_path)
         resized_image = original_image.resize((400, 400), Image.ANTIALIAS)
         return ImageTk.PhotoImage(resized_image)
-    def load_and_resize_image2(self, image_path):
-        original_image = Image.open(image_path)
-        resized_image = original_image.resize((400, 400), Image.ANTIALIAS)
-        return ImageTk.PhotoImage(resized_image)
 
-    def show_answer(self,event=None):
-        self.flashcard_image = self.load_and_resize_image2(flashcards[self.current_flashcard]["pregunta"])
-        self.flashcard_label.config(image=self.flashcard_image)
-                
-        self.flashcard_image2 = self.load_and_resize_image2(flashcards[self.current_flashcard]["respuesta"])
-        self.flashcard_label2.config(image=self.flashcard_image2)
-
-
-        self.showing_answer = True
-#wd
-    def next_question(self,event=None):
-        
-            self.showing_answer = False
-            self.flashcard_image2 = self.load_and_resize_image("F:/programacion/flashcards/images/NotasPentagrama/Cuadrado.png")
-
+    def show_answer(self, event=None):
+            self.flashcard_image2 = self.load_and_resize_image(flashcards[self.current_flashcard]["respuesta"])
             self.flashcard_label2.config(image=self.flashcard_image2)
-            
-            #self.current_flashcard += 1
-            #print(len(flashcards))
-            self.current_flashcard=random.randint(0, len(flashcards)-1)
-            #if self.current_flashcard >= len(flashcards):
-            #    self.current_flashcard = 0
-            self.flashcard_image = self.load_and_resize_image(flashcards[self.current_flashcard]["pregunta"])
-            self.flashcard_label.config(image=self.flashcard_image)
-            
+            self.showing_answer = True
+
+    def next_question(self, event=None):
+        self.showing_answer = False
+        self.current_flashcard = random.randint(0, len(flashcards) - 1)
+        self.flashcard_image = self.load_and_resize_image(flashcards[self.current_flashcard]["pregunta"])
+        self.flashcard_label.config(image=self.flashcard_image)
+
+        self.flashcard_image2 = self.load_and_resize_image("F:/programacion/flashcards/images/NotasPentagrama/Cuadrado.png")
+        self.flashcard_label2 = tk.Label(root, image=self.flashcard_image2)
+        self.flashcard_label2.pack()
 
 if __name__ == "__main__":
     root = tk.Tk()
